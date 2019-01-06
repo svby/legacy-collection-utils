3e2e370f46bc81ccd0e7289c210baac1afbe673a
notiocide <stuhlmeier02@gmail.com>
2019-01-06 14:15:32 +0100

    Add documentation
    Signed-off-by: notiocide <stuhlmeier02@gmail.com>

--- src/main/kotlin/net/notiocide/legacyutil/Dictionaries.kt
+++ src/main/kotlin/net/notiocide/legacyutil/Dictionaries.kt
@@ -20,20 +20,41 @@ private class MapDictionary<K : Any, V : Any>(private val map: Map<K, V>) : Dict
 
 private val emptyDictionary = MapDictionary(emptyMap())
 
+/** Returns an empty read-only [Dictionary]. */
 fun <K : Any, V : Any> dictionaryOf(): Dictionary<K, V> =
     @Suppress("UNCHECKED_CAST") (emptyDictionary as Dictionary<K, V>)
 
+/**
+ * Returns a new read-only [Dictionary] with the specified contents, given as a list of [Pair]s where the first
+ * component is the key and the second is the value.
+ *
+ * If multiple pairs have the same key, the resulting dictionary will contain the value from the last of those
+ * pairs.
+ */
 fun <K : Any, V : Any> dictionaryOf(vararg pairs: Pair<K, V>): Dictionary<K, V> =
     MapDictionary(mapOf(*pairs))
 
+/** Returns an empty new [Dictionary]. */
 fun <K : Any, V : Any> mutableDictionaryOf(): Dictionary<K, V> =
     hashtableOf()
 
+/**
+ * Returns a new [Dictionary] with the specified contents, given as a list of [Pair]s where the first component is the
+ * key and the second is the value.
+ *
+ * If multiple pairs have the same key, the resulting dictionary will contain the value from the last of those pairs.
+ */
 fun <K : Any, V : Any> mutableDictionaryOf(vararg pairs: Pair<K, V>): Dictionary<K, V> =
     hashtableOf(*pairs)
 
+/**
+ * Returns a new read-only [Dictionary] containing all key-value pairs from the original map.
+ */
 fun <K : Any, V : Any> Map<K, V>.toDictionary(): Dictionary<K, V> =
     MapDictionary(this)
 
+/**
+ * Returns a new [Dictionary] containing all key-value pairs from the original map.
+ */
 fun <K : Any, V : Any> Map<K, V>.toMutableDictionary(): Dictionary<K, V> =
     toHashtable()
--- src/main/kotlin/net/notiocide/legacyutil/Enumerations.kt
+++ src/main/kotlin/net/notiocide/legacyutil/Enumerations.kt
@@ -9,4 +9,9 @@ internal class IteratorEnumeration<E>(private val iterator: Iterator<E>) : Enume
 
 }
 
+/**
+ * Returns an [Enumeration] that traverses the remaining elements covered by this [Iterable].
+ *
+ * Traversal is undefined if any methods are called on this iterable after the call to asEnumeration.
+ */
 fun <T> Iterable<T>.asEnumeration(): Enumeration<T> = IteratorEnumeration(iterator())
